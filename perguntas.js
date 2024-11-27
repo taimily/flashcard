807678eb4356c090f0c44a9fd777316f0dd8e487
taimily <taimily09@gmail.com>
2024-11-27 11:04:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual maior orgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é a raiz de 144?',
+    '12'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz água em Inglês?',
+    'Hatter'
+)
